1e56957735e6c8ebbf15fbe00080e74ec8757992
afbarboza <alex.barboza@usp.br>
2016-07-16 15:35:34 -0300

    multiboot header fixed

--- boot.s
+++ boot.s
@@ -1,26 +1,34 @@
 .set		MAGIC,		0x1BADB002
 .set		FLAGS,		0 | 1
 .set		CHECKSUM,	-(MAGIC + FLAGS)
+.set		STACK_SIZE,	0x4000
 
-.section	.multiboot
-.align		4
+/*.section	.multiboot
+.align		4, 0
 .long		MAGIC
 .long		FLAGS
-.long		CHECKSUM
-
-.section	.bootstrap_stack, "aw", @nobits
-stack_bottom:
-.skip	16384
-stack_top:
+.long		CHECKSUM */
 
 .section	.text
-.global		_start
-.type		_start, @function
-_start:
-	mov	$stack_top, %esp
-	call	kernel_main
-	cli
-1:	hlt
-	jmp	1b
+.globl		start, _start
 
-.size _start, . - _start
+start:
+_start:
+	jmp	multiboot_entry
+	.align	4
+multiboot_header:
+	.long	MAGIC
+	.long	FLAGS
+	.long	CHECKSUM
+
+multiboot_entry:
+	movl	$(stack + STACK_SIZE), %esp
+	pushl	$0
+	popf
+	pushl	%ebx
+	pushl	%eax
+	call	kernel_main
+loop:	hlt
+	jmp	loop
+
+	.comm	stack, STACK_SIZE
